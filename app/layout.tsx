42fab899c0d20ccb8a528bfe41361f3031a03b67
Sirbu Nicolae <nicksirbu0001@gmail.com>
2024-09-04 16:50:33 +0300

    day one

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { relative } from "path";
+import { Footer, Navbar } from "./components";
+
+
+
+export const metadata: Metadata = {
+  title: "Car Hub",
+  description: "Discover the best cars in the world.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
